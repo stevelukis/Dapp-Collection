242363ec42205d38acecdbc812ccbf48158b2ab4
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-01 17:52:19 +0800

    Fix spacing

--- contracts/24_SplitPayment.sol
+++ contracts/24_SplitPayment.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
+
 contract SplitPayment {
     address public owner;
 
@@ -8,7 +9,6 @@ contract SplitPayment {
     }
 
     function send(address payable[] memory to, uint[] memory amount) public payable onlyOwner {
-
         require(to.length == amount.length, 'to and amount array must have same length.');
         for (uint i = 0; i < to.length; i++) {
             to[i].transfer(amount[i]);
