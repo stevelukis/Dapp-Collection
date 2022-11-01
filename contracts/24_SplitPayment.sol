7c76d6cc36d17589b9b0832794e59c681002e017
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-01 17:49:30 +0800

    Initial commit

---
+++ contracts/24_SplitPayment.sol
@@ -0,0 +1,22 @@
+pragma solidity ^0.8.0;
+
+contract SplitPayment {
+    address public owner;
+
+    constructor (address _owner) {
+        owner = _owner;
+    }
+
+    function send(address payable[] memory to, uint[] memory amount) public payable onlyOwner {
+
+        require(to.length == amount.length, 'to and amount array must have same length.');
+        for (uint i = 0; i < to.length; i++) {
+            to[i].transfer(amount[i]);
+        }
+    }
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only owner can call this function");
+        _;
+    }
+}
