589614bc5fe9fb3f4bc8cc58eeb4421ca08564ec
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-02 15:50:37 +0800

    Add Deed smart contract

---
+++ contracts/Deed.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Deed {
+    address public lawyer;
+    address payable public beneficiary;
+    uint256 public earliest;
+
+    constructor(address _lawyer, address payable _beneficiary, uint fromNow) payable {
+        lawyer = _lawyer;
+        beneficiary = _beneficiary;
+        earliest = block.timestamp + fromNow;
+    }
+
+    function withdraw() public {
+        require(msg.sender == lawyer, "Only the lawyer can invoke the withdrawal.");
+        require(block.timestamp >= earliest, "It is too early to invoke the withdrawal.");
+        beneficiary.transfer(address(this).balance);
+    }
+}
