5a0b5907c6124513deb66221b1ad6b2f01eb9b80
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-02 15:18:33 +0800

    Change to cleaner code

--- test/24_SplitPayment.test.ts
+++ test/24_SplitPayment.test.ts
@@ -7,37 +7,47 @@ describe("SplitPayment", () => {
     let SplitPayment: ContractFactory;
     let splitPayment: Contract;
 
-    let owner: SignerWithAddress;
-    let address1: SignerWithAddress;
-    let address2: SignerWithAddress;
+    let accounts: SignerWithAddress[];
 
     before(async () => {
-        [owner, address1, address2] = await ethers.getSigners();
+        accounts = await ethers.getSigners();
         SplitPayment = await ethers.getContractFactory("SplitPayment");
     })
 
     beforeEach(async () => {
+        let owner = accounts[0];
         splitPayment = await SplitPayment.deploy(owner.address);
         await splitPayment.deployed();
     })
 
     it("Should split payment correctly", async () => {
-        const startBalanceAddress1 = await address1.getBalance();
-        const startBalanceAddress2 = await address2.getBalance();
+        const recipients = [accounts[1], accounts[2], accounts[3]];
+        const to = recipients.map(recipient => recipient.address);
+        const amounts = [100, 200, 300];
 
-        const to = [address1.address, address2.address];
-        const amount = [100, 200];
-
-        const endBalanceAddress1 = startBalanceAddress1.add(amount[0]);
-        const endBalanceAddress2 = startBalanceAddress2.add(amount[1]);
+        // Get the balances before calling the function
+        const initialBalances = await Promise.all(recipients.map(recipient => {
+            return recipient.getBalance();
+        }));
 
         // Total of amount
-        const value = amount.reduce((prev, current) => prev + current);
+        const value = amounts.reduce((prevAmount, currentAmount) => prevAmount + currentAmount);
 
-        await splitPayment.send(to, amount, {value});
+        // Call the function
+        await splitPayment.send(to, amounts, {value});
 
-        expect(await address1.getBalance()).to.be.equal(endBalanceAddress1);
-        expect(await address2.getBalance()).to.be.equal(endBalanceAddress2);
+        // Get the balance after calling the function
+        const endBalances = await Promise.all(recipients.map(recipient => {
+            return recipient.getBalance();
+        }));
+
+        recipients.forEach((recipient, index) => {
+            const initialBalance = initialBalances[index];
+            const endBalance = endBalances[index];
+
+            // The difference should be equal to the amount for the recipient
+            expect(endBalance.sub(initialBalance)).to.be.equal(amounts[index])
+        });
     });
 
 });
