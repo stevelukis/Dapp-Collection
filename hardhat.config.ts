1df9d7d892e6432a48eda4eca1a5e0c97da7e443
Steve Lukis <20234730+stevelukis@users.noreply.github.com>
2022-11-02 10:24:15 +0800

    Migrate to typescript

---
+++ hardhat.config.ts
@@ -0,0 +1,10 @@
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-ethers";
+import { HardhatUserConfig } from "hardhat/config";
+
+/** @type import('hardhat/config').HardhatUserConfig */
+const config: HardhatUserConfig = {
+    solidity: "0.8.4"
+};
+
+export default config;
